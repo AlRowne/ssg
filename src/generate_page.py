285f7664e8f6c91c04da6ca136af36666f4b2014
AlRowne <aroth@mailbox.org>
2026-06-21 20:16:08 +0200

    Add recursive page generation and contact page
    - Add support for recursive generation of markdown content into HTML
    - Create contact page at content/contact/index.md
    - Update main to use generate_pages_recursive and import it
    - Comment out previous per-page generate_page calls in main

--- src/generate_page.py
+++ src/generate_page.py
@@ -27,6 +27,38 @@ def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
 
     if not os.path.isdir(os.path.dirname(dest_path)):
         os.makedirs(os.path.dirname(dest_path))
+        print(f"created directory '{os.path.dirname(dest_path)}'")
 
     with open(dest_path, "w") as f:
         _ = f.write(final_html)
+
+
+def generate_pages_recursive(
+    dir_path_content: str, template_path: str, dest_dir_path: str
+) -> None:
+    if not os.path.isdir(dir_path_content):
+        raise ValueError(f"{dir_path_content} is not a valid directory")
+
+    print(f"content of {dir_path_content}: '{os.listdir(dir_path_content)}'")
+
+    for entry in os.listdir(dir_path_content):
+        source_path = os.path.join(dir_path_content, entry)
+        dest_path = os.path.join(dest_dir_path, entry)
+
+        if os.path.isfile(source_path) and entry.lower().endswith(".md"):
+            html_dest_path = os.path.splitext(dest_path)[0] + ".html"
+            print(f"{source_path} is a file, generating page {html_dest_path}")
+            generate_page(source_path, template_path, html_dest_path)
+
+        elif os.path.isdir(source_path):
+            print(
+                f"{source_path} is a dir, calling recursively with {source_path}, {template_path} and {dest_path}"
+            )
+            generate_pages_recursive(
+                source_path,
+                template_path,
+                dest_path,
+            )
+
+        else:
+            print(f"{source_path} is not a valid .md file")
--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import os
 import shutil
 
 from copy_files import copy_files
-from generate_page import generate_page
+from generate_page import generate_page, generate_pages_recursive
 
 
 def main():
@@ -12,20 +12,22 @@ def main():
 
     copy_files("static/", "public/")
 
-    generate_page("content/index.md", "template.html", "public/index.html")
-    generate_page(
-        "content/blog/glorfindel/index.md",
-        "template.html",
-        "public/blog/glorfindel/index.html",
-    )
-    generate_page(
-        "content/blog/majesty/index.md",
-        "template.html",
-        "public/blog/majesty/index.html",
-    )
-    generate_page(
-        "content/blog/tom/index.md", "template.html", "public/blog/tom/index.html"
-    )
+    generate_pages_recursive("content/", "template.html", "public/")
+
+    # generate_page("content/index.md", "template.html", "public/index.html")
+    # generate_page(
+    #     "content/blog/glorfindel/index.md",
+    #     "template.html",
+    #     "public/blog/glorfindel/index.html",
+    # )
+    # generate_page(
+    #     "content/blog/majesty/index.md",
+    #     "template.html",
+    #     "public/blog/majesty/index.html",
+    # )
+    # generate_page(
+    #     "content/blog/tom/index.md", "template.html", "public/blog/tom/index.html"
+    # )
 
 
 if __name__ == "__main__":
