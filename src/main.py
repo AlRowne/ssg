4e82930868acd6bba1ebb75a9e45ac471588b0a5
AlRowne <aroth@mailbox.org>
2026-06-21 19:24:16 +0200

    Add three new blog posts and images

--- src/main.py
+++ src/main.py
@@ -13,6 +13,19 @@ def main():
     copy_files("static/", "public/")
 
     generate_page("content/index.md", "template.html", "public/index.html")
+    generate_page(
+        "content/blog/glorfindel/index.md",
+        "template.html",
+        "public/blog/glorfindel/index.html",
+    )
+    generate_page(
+        "content/blog/majesty/index.md",
+        "template.html",
+        "public/blog/majesty/index.html",
+    )
+    generate_page(
+        "content/blog/tom/index.md", "template.html", "public/blog/tom/index.html"
+    )
 
 
 if __name__ == "__main__":
